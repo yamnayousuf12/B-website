0d6caa0b146de4647b3d74a22bfb1e3fbb739f54
yamnayousuf12 <154628218+yamnayousuf12@users.noreply.github.com>
2025-07-18 21:53:38 +0500

    Changes

--- src/app/contact-us/ContactInfo.tsx
+++ src/app/contact-us/ContactInfo.tsx
@@ -20,7 +20,7 @@ const StatsSection = () => {
 
 
         </p>
-        <p className="text-sm md:text-lg font-light mb-4 text-gray1">We hear you! Let&apos;s collaborate to find  <span className="text-white font-medium">managed IT solutions </span>for your business IT queries. Our team is ready to craft custom apps, AI solutions, or secure Shopify stores that drive your success.</p>
+        <p className="text-sm md:text-lg font-light mb-4 text-gray1">We hear you! Let&apos;s collaborate to find  <span className="text-white font-medium">managed IT solutions </span>for your business IT queries. Our team is ready to craft custom apps, AI solutions, or secure shopify stores that drive your success.</p>
         <p className="text-sm md:text-lg font-light mb-4 text-gray1">
 Complete the form or send us an email.</p>
         
@@ -53,37 +53,51 @@ Complete the form or send us an email.</p>
             <h2 className="text-2xl font-bold mb-4 text-white">Send a Message</h2>
             <form className="space-y-4">
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <input
-                  type="text"
-                  placeholder="Name"
-                  className="bg-white/15  text-white p-3 rounded outline-none"
-                />
-                <input
-                  type="email"
-                  placeholder="Email"
-                  className="bg-white/15  text-white p-3 rounded outline-none"
-                />
+                <div>
+                  <label htmlFor="Name" className="block mb-1 text-white">Name</label>
+                  <input
+                  id="Name"
+                  type="Name"
+                  className="bg-white/15 text-white p-3 rounded outline-none border border-white"/>
+                  </div>
+
+                <div>
+                  <label htmlFor="email" className="block mb-1 text-white">Email</label>
+                  <input
+                   id="email"
+                   type="email"
+                   className="bg-white/15 text-white p-3 rounded outline-none border border-white"/>
+                   </div>
+
               </div>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+               <div>
+                <label htmlFor="Company" className="block mb-1 text-white">Company</label>
                 <input
-                  type="text"
-                  placeholder="Company"
-                  className="bg-white/15  text-white p-3 rounded outline-none"
-                />
-                <input
-                  type="text"
-                  placeholder="Phone"
-                  className="bg-white/15  text-white p-3 rounded outline-none"
-                />
-              </div>
-              <textarea
-                placeholder="Message"
-                rows={5}
-                className="w-full bg-white/15  text-white p-3 rounded outline-none"
-              />
+                id="Company"
+                type="Company"
+                className="bg-white/15 text-white p-3 rounded outline-none border border-white" />
+                </div>
+                <div>
+                  <label htmlFor="Phone" className="block mb-1 text-white">Phone</label>
+                  <input 
+                  id="Phone"
+                  type="Phone"
+                  className="bg-white/15 text-white p-3 rounded outline-none border border-white"/>
+                  </div>
+                  </div>
+                  <div>
+                    <label htmlFor="message" className="block mb-1 text-white">Message</label>
+                    <textarea
+                    id="message"
+                    placeholder="Message"
+                    rows={5}
+                    className="w-full bg-white/15 text-white border border-white p-3 rounded outline-none"/>
+                    </div>
+
               <button
                 type="submit"
-                className="bg-Blue text-white px-6 py-3 rounded hover:bg-gray transition-colors"
+                className="bg-Blue text-white md:px-6 md:py-3 px-3 py-1 font-medium hover:bg-Blue flex items-center gap-2 border border-gray rounded-full ml-24  md:ml-0"
               >
                 Send Message
               </button>
--- src/app/contact-us/Contacthero.tsx
+++ src/app/contact-us/Contacthero.tsx
@@ -18,15 +18,15 @@ const Hero = () => {
     Contact 
     </div>
     </div>
-<h1 className="text-sm md:text-4xl md:mr-44 fount-semibold md-text-justify text-center text-gray1">
+<h1 className="text-xl md:text-4xl md:mr-44 fount-semibold md-text-justify text-center text-gray1">
 Lets Create Something Extraordinary with Us</h1>
-        <p className="text-gray1 text-sm md:text-lg mb-8 md:ml-10">
+        <p className="text-gray1 text-xs md:text-lg mb-8 md:ml-10">
           Ready to transform your business? Reach out, we are here to help
         </p>
 
          <div className="flex flex-col md:flex-row gap-4 justify-center md:justify-start md:ml-10">
           <Link href="/contact-us">
-            <button className="bg-Blue/30 text-white md:px-6 md:py-3 px-3 py-1 font-medium hover:bg-Blue flex items-center gap-2 border border-gray rounded-full ml-24  md:ml-0">
+            <button className="bg-Blue text-white md:px-6 md:py-3 px-3 py-1 font-medium hover:bg-Blue flex items-center gap-2 border border-gray rounded-full ml-24  md:ml-0">
                <PhoneCall className="md:text-xl text-sm md:w-7 w-4" />
               Contact Now 
             </button>
