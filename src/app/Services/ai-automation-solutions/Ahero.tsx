4f9ec5ec799203cd4c978da7acc1a02c851d04bd
yamnayousuf12 <154628218+yamnayousuf12@users.noreply.github.com>
2025-08-01 00:32:30 +0500

    Changes

--- src/app/Services/ai-automation-solutions/Ahero.tsx
+++ src/app/Services/ai-automation-solutions/Ahero.tsx
@@ -4,14 +4,14 @@
 
 const Hui = () => {
   return (
-    <section className="bg-black text-white md:px-6 md:py-20 px-3 py-5 text-center md:text-left relative"
+    <section className="bg-black text-white md:px-6 md:py-20 px-3 py-10 text-center md:text-left relative md:h-[500px]  overflow-hidden bg-cover bg-center bg-no-repeat md:max-w-7xl mx-auto"
     style={{
         backgroundImage: "url('/about.png')", // Optional background wave
       }} >
 
     
 
-      <div className="max-w-5xl mx-auto  ">
+      <div className="max-w-5xl mx-auto mt-20">
         <div className="mb-8 flex justify-center md:justify-start">
        <div className=" inline-flex items-center gap-2 md:px-4 md:py-1.5 px-2 py-2 rounded-full bg-Blue text-white text-sm font-medium shadow-sm">
       
@@ -24,14 +24,7 @@ Mobile App Development </h1>
          Streamline your operating systems with high-performance mobile and web applications
         </p>
 
-        {/* <div className="flex flex-col md:flex-row gap-4 justify-center md:justify-start">
-          <Link href="/contact-us">
-            <button className="bg-white text-black md:px-6 md:py-3 px-3 py-1 rounded-lg font-medium hover:bg-Blue">
-              Contact Us →
-            </button>
-          </Link>
-          
-        </div> */}
+    
       </div>
     </section>
   );
